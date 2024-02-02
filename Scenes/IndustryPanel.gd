984b55d3ed3f3db908d2b3029bdafa54b0c100dc
unknown <dwlee0802@gmail.com>
2024-02-02 23:52:37 +0900

    Production progress bar shows left time.

--- Scenes/IndustryPanel.gd
+++ Scenes/IndustryPanel.gd
@@ -16,22 +16,6 @@ func _process(delta):
 	if industry != null:
 		nameLabel.text = industry.name
 		levelLabel.text = str(industry.level)
-		
-		
-func SetProductionProgress(value):
-	productionProgressBar.value = value
-	productionProgressLabel.text = str(int(value * 100) * 0.01)
-
-
-func SetProductionProgressMax(value):
-	productionProgressBar.max_value = value
-	
-	
-func SetIngredientProgress(value):
-	ingredientProgressBar.value = value
-	ingredientProgressLabel.text = str(int(value * 100) * 0.01)
-	
-	
-func SetIngredientProgressMax(value):
-	ingredientProgressBar.max_value = value
-	
+		productionProgressBar.max_value = industry.productionTimer.wait_time
+		productionProgressBar.value = industry.productionTimer.wait_time - industry.productionTimer.time_left
+		productionProgressLabel.text = str(industry.productionTimer.time_left)
