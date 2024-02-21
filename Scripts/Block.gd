74f5bba8922fe1c7792be4196461078733a66e39
unknown <dwlee0802@gmail.com>
2024-02-21 22:41:13 +0900

    Blocks now keep track of how much of combat width is being used.

--- Scripts/Block.gd
+++ Scripts/Block.gd
@@ -3,7 +3,12 @@ class_name Block
 
 @onready var detectionArea: Area2D = $DetectionArea/Area2D
 
+var curCombatWidth: int = 0
 var maxCombatWidth: int = 10
+
+@onready var contentsLabel: RichTextLabel = $ContentsLabel
+
+var insideUnits
 
 
 # Called when the node enters the scene tree for the first time.
@@ -13,10 +18,42 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(len(detectionArea.get_overlapping_bodies()))
+	insideUnits = GetUnitsInside()
+	UpdateContentsLabel()
 
 
 func UpdateContentsLabel():
 	var output = ""
+	
+	# count units on this block for each type
+	var countDict = {}
+	
+	for unit in insideUnits:
+		if unit is Unit:
+			if countDict.has(unit.unitData.unitType):
+				countDict[unit.unitData.unitType] += 1
+			else:
+				countDict[unit.unitData.unitType] = 1
+	
+	for key in countDict.keys():
+		output += "[fill]" + Enums.UnitTypeToAbbString(key) + ": " + str(countDict[key]) + "[/fill]\n"
+	
+	if len(countDict.keys()) != 0:
+		output += "[fill]Total: "+ str(curCombatWidth) + "/" + str(maxCombatWidth) + "[/fill]\n"
+	
+	contentsLabel.text = output
+
+
+# units are considered 'on' this block only if their position is equal or greater than self's width
+# specifically, unit's position should be inside [self.global_position, self.global_position + size.x)
+func GetUnitsInside():
+	var results = detectionArea.get_overlapping_bodies()
+	var output = []
+	for unit in results:
+		if unit is Unit:
+			if unit.global_position.x >= global_position.x and unit.global_position.x < global_position.x + size.x:
+				output.append(unit)
+	
+	curCombatWidth = len(output)
 	
 	return output
--- Scripts/Enums.gd
+++ Scripts/Enums.gd
@@ -23,6 +23,30 @@ enum IndustrySector {Basic, Processed, Manufactured, Recruitment}
 
 
 enum UnitType {Infantry, Artillery, Armored, Antitank}
+static var UNIT_TYPE_COUNT: int = 4
+static func UnitTypeToString(num):
+	if num == 0:
+		return "Infantry"
+	if num == 1:
+		return "Artillery"
+	if num == 2:
+		return "Armored"
+	if num == 3:
+		return "Antitank"
+	
+	return "ERROR!"
+	
+static func UnitTypeToAbbString(num):
+	if num == 0:
+		return "Inf"
+	if num == 1:
+		return "Arty"
+	if num == 2:
+		return "Armd"
+	if num == 3:
+		return "AT"
+	
+	return "ERROR!"
 
 
 enum OrderType {Offensive, Defensive, Retreat}
